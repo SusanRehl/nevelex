7cff248b4954d3c9873a2148f11ad41127d6c45d
Susan Rehl <susanrehl@gmail.com>
2017-03-20 11:34:55 -0500

    Files set up. Hello World achieved.

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+
+app.use(express.static('public'));
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'public/views/index.html'));
+});
+
+app.listen(5000);
